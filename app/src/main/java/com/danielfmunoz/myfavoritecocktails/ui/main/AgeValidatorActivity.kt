295d26a4d1adb87cefb0f189564eba8b71f3a290
DANIEL MUNOZ <dafemure.180@gmail.com>
2023-03-16 16:30:27 -0500

    Se actualizan los textos quemados para que sean dinamicos y dependientes del strings.xml

--- app/src/main/java/com/danielfmunoz/myfavoritecocktails/ui/main/AgeValidatorActivity.kt
+++ app/src/main/java/com/danielfmunoz/myfavoritecocktails/ui/main/AgeValidatorActivity.kt
@@ -29,7 +29,7 @@ class AgeValidatorActivity : AppCompatActivity() {
                 val intent = Intent(this, MainActivity::class.java)
                 startActivity(intent)
             } else {
-                Toast.makeText(this, "Debes ser mayor de edad para continuar", Toast.LENGTH_SHORT).show()
+                Toast.makeText(this, getString(R.string.adulto), Toast.LENGTH_SHORT).show()
             }
         }
     }
